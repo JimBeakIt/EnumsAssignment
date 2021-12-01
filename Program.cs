6dca06683c9c4b645a058c160a896e0bd00ccf0b
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-12-01 08:02:23 +0000

    update

--- Program.cs
+++ Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace EnumsAssignment
 {
+    enum DaysOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Enter the current day.");
+                string day1 = Console.ReadLine();
+
+                DaysOfWeek day = (DaysOfWeek)Convert.ToInt32(day1);
+
+                Console.WriteLine(day);
+
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+
+                Console.ReadLine();
+            }
         }
     }
 }
